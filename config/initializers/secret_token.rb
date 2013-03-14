57723a27ebbfaa5c78866d54e45abe711ebee120
Tom Harris <thomas.e.harris@gmail.com>
2013-03-14 19:43:13 -0400

    ran rails generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeezeoDashboard::Application.config.secret_token = 'f5314c9fdea44de3a81dad11d3e4dbe3213c53f02eab8ab4f345c5e74b7900b6584042407a9e9e3ae40d94b622e8ac3899418aab9f62670eeac678626862c839'
