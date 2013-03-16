3b5968b27f77738837a0384ba5a96a903d735de1
Tom Harris <thomas.e.harris@gmail.com>
2013-03-15 22:12:28 -0400

    Added call to accounts api to load all the accounts for a given user.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,23 @@
 class Account
-  attr_reader :id, :name, :balance, :reference_id, :aggregation_type, :state
+  include HTTParty
+  attr_accessor :id, :name, :balance, :reference_id, :aggregation_type, :state
+
+  base_uri GeezeoDashboard::Application.config.geezeo_api_base_uri
+
+  def self.find_all_for(user)
+    get("/users/#{user.user_id}/accounts", basic_auth: { username: user.token }).parsed_response['accounts'].collect do |attributes|
+      from_hash(attributes)
+    end
+  end
+
+  def self.from_hash(attributes)
+    new.tap do |account|
+      account.id                = attributes['id']
+      account.name              = attributes['name']
+      account.balance           = attributes['balance']
+      account.reference_id      = attributes['reference_id']
+      account.aggregation_type  = attributes['aggregation_type']
+      account.state             = attributes['state']
+    end
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -7,4 +7,46 @@ describe Account do
       Account.new.should respond_to(field_name)
     end
   end
+
+  describe "::from_hash" do
+
+    before(:each) do
+      @attributes = {
+        'id'                => 123,
+        'name'              => 'my account',
+        'balance'           => 10.01,
+        'reference_id'      => '12',
+        'aggregation_type'  => 'partner',
+        'state'             => 'active'
+      }
+    end
+
+    it "should turn the attributes into an instance" do
+      Account.from_hash(@attributes).should be_instance_of(Account)
+    end
+
+    it "should set the fields in the instance to the values in the attributes" do
+      instance = Account.from_hash(@attributes)
+      instance.id.should                == 123
+      instance.name.should              == 'my account'
+      instance.balance.should           == 10.01
+      instance.reference_id.should      == '12'
+      instance.aggregation_type.should  == 'partner'
+      instance.state.should             == 'active'
+    end
+  end
+
+  describe "::find_all_for" do
+
+    before(:each) do
+      @user = User.new('123', 'some_token')
+    end
+
+    it "should perform a GET request to the accounts api to fetch the accounts" do
+      response = mock("mock response").as_null_object
+      Account.should_receive(:get).with('/users/123/accounts', hash_including({ basic_auth: { username: 'some_token' } })).and_return(response)
+
+      Account.find_all_for(@user)
+    end
+  end
 end
