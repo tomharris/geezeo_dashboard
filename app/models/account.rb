bfd4a31aba20f6373a64d57d8436ee1d8828e735
Tom Harris <thomas.e.harris@gmail.com>
2013-03-17 12:00:00 -0400

    added method for accessing transactions for an account instance

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account
   include HTTParty
-  attr_accessor :id, :name, :balance, :reference_id, :aggregation_type, :state
+  attr_accessor :user, :id, :name, :balance, :reference_id, :aggregation_type, :state
 
   base_uri GeezeoDashboard::Application.config.geezeo_api_base_uri
 
@@ -8,7 +8,7 @@ class Account
 
   def self.find_all_for(user)
     get(resource_path_pattern.gsub(':user_id', user.user_id), basic_auth: { username: user.token }).parsed_response['accounts'].collect do |attributes|
-      from_hash(attributes)
+      from_hash(attributes).tap { |account| account.user = user }
     end
   end
 
@@ -21,5 +21,9 @@ class Account
       account.aggregation_type  = attributes['aggregation_type']
       account.state             = attributes['state']
     end
+  end
+
+  def transactions
+    @transactions ||= Transaction.find_all_for(self.user, self)
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe Account do
 
+  before(:each) do
+    WebMock.reset!
+    stub_geezeo_api_requests
+
+    @user = User.new('123', 'some_token')
+  end
+
   it "should have the right fields" do
     %w(id name balance reference_id aggregation_type state).each do |field_name|
       Account.new.should respond_to(field_name)
@@ -38,16 +45,36 @@ describe Account do
 
   describe "::find_all_for" do
 
-    before(:each) do
-      WebMock.reset!
-      stub_geezeo_api_requests
-
-      @user = User.new('123', 'some_token')
-    end
-
     it "should perform a GET request to the accounts api to fetch the accounts" do
       Account.find_all_for(@user)
       a_request_of_accounts_for(@user).should have_been_made
+    end
+
+    it "should also assign the user to an accessor" do
+      Account.find_all_for(@user).first.user.should == @user
+    end
+  end
+
+  describe "#transactions" do
+
+    before(:each) do
+      @account = Account.new.tap { |a| a.id = '321'; a.user = @user }
+    end
+
+    it "should have requested this account's transactions" do
+      @account.transactions
+      a_request_of_transactions_for(@user, @account).should have_been_made
+    end
+
+    it "should only request the accounts once when called multiple times" do
+      @account.transactions
+      @account.transactions
+      a_request_of_transactions_for(@user, @account).should have_been_made.once
+    end
+
+    it "should return an array of Transaction" do
+      @account.transactions.should respond_to(:each)
+      @account.transactions.first.should be_kind_of(Transaction)
     end
   end
 end
